68ed7cb385b42052f4e316d780fb6bfc6aa3fbcf
Sergey Sova <i.am@lestad.net>
2017-05-05 01:57:14 +0300

    Fetch links from posts

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 const cheerio = require('cheerio')
 const axios = require('axios').default
 const R = require('ramda')
+const { readFile, writeFile } = require('pify')(require('fs'))
 
 const URL_BASE = 'https://this-week-in-rust.org/'
+const FILE_NAME = './lastId.txt'
+
+let latestId = 0
+
+const saveId = () => writeFile(FILE_NAME, latestId)
 
 const attr = R.curry((name, el) => cheerio(el).attr(name))
 const mapOver = R.curry((path, fn, el) => el(path).map(R.flip(fn)).get())
 const toInt = R.flip(parseInt)
+const moreThan = R.curry((a, b) => a > b)
 
-axios.get(URL_BASE)
+const getIdFromLink = R.pipe(
+  R.split('-'),
+  R.last,
+  R.replace('\/', ''),
+  toInt(10)
+)
+
+readFile(FILE_NAME, 'utf8')
+  .then(content => {
+    latestId = toInt(10, content)
+    console.log('loaded latest id', latestId)
+  }, saveId)
+  .then(() => axios.get(URL_BASE))
+  // fetch posts
   .then(R.pipe(
     R.prop('data'),
     cheerio.load,
     mapOver('.row.post-title a', attr('href')),
-    R.map(R.pipe(
-      R.split('-'),
-      R.last,
-      R.replace('\/', ''),
-      toInt(10)
-    )),
-    R.tap(console.log)
+    // select post link and id
+    R.map(
+      R.converge(
+        (link, id) => ({ link, id }),
+        [e => e, getIdFromLink]
+      )
+    ),
+    // load only new
+    R.filter(
+      R.pipe(
+        R.prop('id'),
+        moreThan(R.__, latestId)
+      )
+    ),
+    // update latestId and fetch each post
+    R.map(i => (
+      latestId = i.id > latestId ? i.id : latestId,
+      axios.get(i.link)
+    ))
   ))
+  .then(all => Promise.all(all))
+  // fetch links from each post
+  .then(R.map(R.pipe(
+    R.prop('data'),
+    cheerio.load,
+    $ => $('.page-content .post-content ul').first().find('li a').map(R.flip(cheerio)).get(),
+    R.map(e => ({ text: e.text(), link: e.attr('href')})),
+    R.flatten
+  )))
+  .then(R.flatten)
+  .then(R.tap(console.log))
+
